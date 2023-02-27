0d0ee473840aefdd899f2256770dbce671eb36cd
Alcatraz1221 <111422710+Alcatraz121@users.noreply.github.com>
2023-02-27 15:06:08 +0000

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,15 +10,15 @@ char *_strcpy(char *dest, char *src)
     int i = 0 ;
     int count = 0 ;
     while(src[i] != '\0')
-    {
+{
         i++;
         count++;
-    }
+}
     for(int i  =0 ;i < count ; i ++)
-    {
-       dest[i] = src[i] ;
+{
+        dest[i] = src[i] ;
 
-    }
-    return dest;
+}
+        return dest;
 
 }
