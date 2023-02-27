7f50b4a93545d1d56ce9c7f97c3fa7a2f5f96191
Alcatraz1221 <111422710+Alcatraz121@users.noreply.github.com>
2023-02-27 15:03:42 +0000

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,12 @@
-char *_strcpy(char *dest, char *src){
+#include "main.h"
+/**
+ * _strcpy - copy the string pointed to by src to dest
+ * @dest: char to check
+ * @src: char to check
+ * Return: 0 is success
+ */
+char *_strcpy(char *dest, char *src)
+{
     int i = 0 ;
     int count = 0 ;
     while(src[i] != '\0')
